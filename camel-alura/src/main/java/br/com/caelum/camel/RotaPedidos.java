5861aac647a126114fe2697658253e5d6784c4ac
cleberGraciano <cleber_batista2010@hotmail.com>
2019-04-10 01:46:53 -0300

    Criando validadores de entrada para Xml

--- camel-alura/src/main/java/br/com/caelum/camel/RotaPedidos.java
+++ camel-alura/src/main/java/br/com/caelum/camel/RotaPedidos.java
@@ -2,6 +2,7 @@ package br.com.caelum.camel;
 
 import org.apache.camel.CamelContext;
 import org.apache.camel.Exchange;
+import org.apache.camel.Processor;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.component.http4.HttpMethods;
 import org.apache.camel.impl.DefaultCamelContext;
@@ -15,12 +16,28 @@ public class RotaPedidos {
 
 			@Override
 			public void configure() throws Exception {
-
+				
+				errorHandler(deadLetterChannel("file:erro").
+						logExhaustedMessageHistory(true).
+						maximumRedeliveries(3).//retenta 3 vezes
+							redeliveryDelay(2000).
+								onRedelivery(new Processor() {
+									
+									@Override
+									public void process(Exchange exchange) throws Exception {
+										int counter = (int) exchange.getIn().getHeader(Exchange.REDELIVERY_COUNTER);
+										int max = (int) exchange.getIn().getHeader(Exchange.REDELIVERY_MAX_COUNTER);
+										System.out.println("Redelivery " +counter+"/"+max);
+										
+									}
+								}));// a cada 2 minutos
+				
 				from("file:pedidos?delay=5s&noop=true").
 				routeId("rota-pedidos").
-				multicast(). //Espalha a mesma mensagem para varias rotas 
-					to("direct:soap").
-					to("direct:http");
+				to("validator:pedido.xsd");
+				//multicast(). //Espalha a mesma mensagem para varias rotas 
+					//to("direct:soap").
+					//to("direct:http");
 				
 				
 				from("direct:http").
